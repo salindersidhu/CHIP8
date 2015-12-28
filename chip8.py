c273fcb9d2e754f38fd3c93c4b669b70cc3765bf
Salinder Sidhu <salinder.sid@gmail.com>
2015-12-28 05:23:22 -0500

    More chip8 code re-factoring

--- chip8.py
+++ chip8.py
@@ -30,26 +30,26 @@ class Chip8(object):
                           '80', 'F0', 'E0', '90', '90', '90', 'E0', 'F0', '80',
                           'F0', '80', 'F0', 'F0', '80', 'F0', '80', '80']
         # Opcode instruction jump tables
-        self.__opCodeTable = {'0': self.__inst0x0NNN, '1': self.__inst0x1NNN,
-                              '2': self.__inst0x2NNN, '3': self.__inst0x3XNN,
-                              '4': self.__inst0x4XNN, '5': self.__inst0x5XY0,
-                              '6': self.__inst0x6XNN, '7': self.__inst0x7XNN,
-                              '8': self.__inst0x8NNN, '9': self.__inst0x9XY0,
-                              'a': self.__inst0xANNN, 'b': self.__inst0xBNNN,
-                              'c': self.__inst0xCXNN, 'd': self.__inst0xDXYN,
-                              'e': self.__inst0xENNN, 'f': self.__inst0xFNNN}
-        self.__table0x0NNN = {'e0': self.__inst0x00E0, 'ee': self.__inst0x00EE}
-        self.__table0x8NNN = {'0': self.__inst0x8XY0, '1': self.__inst0x8XY1,
-                              '2': self.__inst0x8XY2, '3': self.__inst0x8XY3,
-                              '4': self.__inst0x8XY4, '5': self.__inst0x8XY5,
-                              '6': self.__inst0x8XY6, '7': self.__inst0x8XY7,
-                              'e': self.__inst0x8XYE}
-        self.__table0xENNN = {'9e': self.__inst0xEX9E, 'a1': self.__inst0xEXA1}
-        self.__table0xFNNN = {'07': self.__inst0xFX07, '0a': self.__inst0xFX0A,
-                              '15': self.__inst0xFX15, '18': self.__inst0xFX18,
-                              '1e': self.__inst0xFX18, '1e': self.__inst0xFX1E,
-                              '29': self.__inst0xFX29, '33': self.__inst0xFX33,
-                              '55': self.__inst0xFX55, '65': self.__inst0xFX65}
+        self.__opCodeTable = {'0': self.__jump0NNN, '1': self.__inst1NNN,
+                              '2': self.__inst2NNN, '3': self.__inst3XNN,
+                              '4': self.__inst4XNN, '5': self.__inst5XY0,
+                              '6': self.__inst6XNN, '7': self.__inst7XNN,
+                              '8': self.__jump8NNN, '9': self.__inst9XY0,
+                              'a': self.__instANNN, 'b': self.__instBNNN,
+                              'c': self.__instCXNN, 'd': self.__instDXYN,
+                              'e': self.__jumpENNN, 'f': self.__jumpFNNN}
+        self.__table0NNN = {'e0': self.__inst00E0, 'ee': self.__inst00EE}
+        self.__table8NNN = {'0': self.__inst8XY0, '1': self.__inst8XY1,
+                            '2': self.__inst8XY2, '3': self.__inst8XY3,
+                            '4': self.__inst8XY4, '5': self.__inst8XY5,
+                            '6': self.__inst8XY6, '7': self.__inst8XY7,
+                            'e': self.__inst8XYE}
+        self.__tableENNN = {'9e': self.__instEX9E, 'a1': self.__instEXA1}
+        self.__tableFNNN = {'07': self.__instFX07, '0a': self.__instFX0A,
+                            '15': self.__instFX15, '18': self.__instFX18,
+                            '1e': self.__instFX18, '1e': self.__instFX1E,
+                            '29': self.__instFX29, '33': self.__instFX33,
+                            '55': self.__instFX55, '65': self.__instFX65}
 
     def reset(self):
         '''Reset the CHIP-8 system to it's original state. Clear all registers,
@@ -138,101 +138,101 @@ class Chip8(object):
             if self.__timers[i] > 0:
                 self.__timers[i] -= 1
 
-    def __inst0x0NNN(self):
-        '''Opcode jump function for table0x0NNN.'''
-        self.__table0x0NNN[self.__opCode[2:4]]()
+    def __jump0NNN(self):
+        '''Opcode jump function for table0NNN.'''
+        self.__table0NNN[self.__opCode[2:4]]()
 
-    def __inst0x8NNN(self):
-        '''Opcode jump function for table0x8NNN.'''
-        self.__table0x8NNN[self.__opCode[3]]()
+    def __jump8NNN(self):
+        '''Opcode jump function for table8NNN.'''
+        self.__table8NNN[self.__opCode[3]]()
 
-    def __inst0xENNN(self):
-        '''Opcode jump function for table0xENNN.'''
-        self.__table0xENNN[self.__opCode[2:4]]()
+    def __jumpENNN(self):
+        '''Opcode jump function for tableENNN.'''
+        self.__tableENNN[self.__opCode[2:4]]()
 
-    def __inst0xFNNN(self):
-        '''Opcode jump function for table0xFNNN.'''
-        self.__table0xFNNN[self.__opCode[2:4]]()
+    def __jumpFNNN(self):
+        '''Opcode jump function for tableFNNN.'''
+        self.__tableFNNN[self.__opCode[2:4]]()
 
-    def __inst0x00E0(self):
+    def __inst00E0(self):
         '''0x00E0: Clear the graphics buffer.'''
         self.__gfx = [[0 for x in range(32)] for y in range(64)]
         self.__pc += 2
 
-    def __inst0x00EE(self):
-        '''0x00EE: Return from subroutine.'''
+    def __inst00EE(self):
+        '''00EE: Return from subroutine.'''
         self.__pc = self.__stk.pop()
         self.__pc += 2
 
-    def __inst0x1NNN(self):
-        '''0x1NNN: Jump to address NNN.'''
+    def __inst1NNN(self):
+        '''1NNN: Jump to address NNN.'''
         self.__pc = int(self.__opCode[1:4], 16)
 
-    def __inst0x2NNN(self):
-        '''0x2NNN: Call subroutine at NNN.'''
+    def __inst2NNN(self):
+        '''2NNN: Call subroutine at NNN.'''
         self.__stk.push(self.__pc)
         self.__pc = int(self.__opCode[1:4], 16)
 
-    def __inst0x3XNN(self):
-        '''0x3XNN: Skip the next instruction if VX equals NN.'''
+    def __inst3XNN(self):
+        '''3XNN: Skip the next instruction if VX equals NN.'''
         if self.__V[int(self.__opCode[1], 16)] == int(self.__opCode[2:4], 16):
             self.__pc += 4
         else:
             self.__pc += 2
 
-    def __inst0x4XNN(self):
-        '''0x4XNN: Skip the next instruction if VX doesn't equal NN.'''
+    def __inst4XNN(self):
+        '''4XNN: Skip the next instruction if VX doesn't equal NN.'''
         if self.__V[int(self.__opCode[1], 16)] != int(self.__opCode[2:4], 16):
             self.__pc += 4
         else:
             self.__pc += 2
 
-    def __inst0x5XY0(self):
-        '''0x5XY0: Skip the next instruction if VX equals VY.'''
+    def __inst5XY0(self):
+        '''5XY0: Skip the next instruction if VX equals VY.'''
         if self.__V[int(self.__opCode[1], 16)] == \
            self.__V[int(self.__opCode[2], 16)]:
             self.__pc += 4
         else:
             self.__pc += 2
 
-    def __inst0x6XNN(self):
-        '''0x6XNN: Set VX to NN.'''
+    def __inst6XNN(self):
+        '''6XNN: Set VX to NN.'''
         self.__V[int(self.__opCode[1], 16)] = int(self.__opCode[2:4], 16)
         self.__pc += 2
 
-    def __inst0x7XNN(self):
-        '''0x7XNN: Add NN to VX.'''
+    def __inst7XNN(self):
+        '''7XNN: Add NN to VX.'''
         self.__V[int(self.__opCode[1], 16)] += int(self.__opCode[2:4], 16)
         self.__V[int(self.__opCode[1], 16)] &= 255  # Take the lowest 8 bits
         self.__pc += 2
 
-    def __inst0x8XY0(self):
-        '''0x8XY0: Set VX to the value of VY.'''
+    def __inst8XY0(self):
+        '''8XY0: Set VX to the value of VY.'''
         self.__V[int(self.__opCode[1], 16)] = \
             self.__V[int(self.__opCode[2], 16)]
         self.__pc += 2
 
-    def __inst0x8XY1(self):
-        '''0x8XY1: Set VX to VX OR VY.'''
+    def __inst8XY1(self):
+        '''8XY1: Set VX to VX OR VY.'''
         self.__V[int(self.__opCode[1], 16)] |= \
             self.__V[int(self.__opCode[2], 16)]
         self.__pc += 2
 
-    def __inst0x8XY2(self):
-        '''0x8XY2: Set VX to VX AND VY.'''
+    def __inst8XY2(self):
+        '''8XY2: Set VX to VX AND VY.'''
         self.__V[int(self.__opCode[1], 16)] &= \
             self.__V[int(self.__opCode[2], 16)]
         self.__pc += 2
 
-    def __inst0x8XY3(self):
-        '''0x8XY3: Set VX to VX XOR VY.'''
+    def __inst8XY3(self):
+        '''8XY3: Set VX to VX XOR VY.'''
         self.__V[int(self.__opCode[1], 16)] ^= \
             self.__V[int(self.__opCode[2], 16)]
         self.__pc += 2
 
-    def __inst0x8XY4(self):
-        '''0x8XY4: Add VY to VX. VF is set to 1 when there's a carry, and to
-        0 when there isn't.'''
+    def __inst8XY4(self):
+        '''8XY4: Add VY to VX. VF is set to 1 when there's a carry, and to 0
+        when there isn't.'''
         self.__V[int(self.__opCode[1], 16)] += \
             self.__V[int(self.__opCode[2], 16)]
         if self.__V[int(self.__opCode[1], 16)] > 255:
@@ -242,9 +242,9 @@ class Chip8(object):
             self.__V[15] = 0
         self.__pc += 2
 
-    def __inst0x8XY5(self):
-        '''0x8XY5: Subtract VY from VX. VF is set to 0 when there's a borrow,
-        and 1 when there isn't.'''
+    def __inst8XY5(self):
+        '''8XY5: Subtract VY from VX. VF is set to 0 when there's a borrow, and
+        1 when there isn't.'''
         if self.__V[int(self.__opCode[1], 16)] > \
            self.__V[int(self.__opCode[2], 16)]:
             self.__V[15] = 1
@@ -254,8 +254,8 @@ class Chip8(object):
             self.__V[int(self.__opCode[2], 16)]
         self.__pc += 2
 
-    def __inst0x8XY6(self):
-        '''0x8XY6: Shift VX right by 1. VF is set to the value of the least
+    def __inst8XY6(self):
+        '''8XY6: Shift VX right by 1. VF is set to the value of the least
         significant bit of VX before the shift.'''
         # Make binary string from integer
         binStr = bin(self.__V[int(self.__opCode[1], 16)])
@@ -268,9 +268,9 @@ class Chip8(object):
         self.__V[int(self.__opCode[1], 16)] >>= 1
         self.__pc += 2
 
-    def __inst0x8XY7(self):
-        '''0x8XY7: Set VX to VY minus VX. VF is set to 0 when there's a
-        borrow, and 1 when there isn't.'''
+    def __inst8XY7(self):
+        '''8XY7: Set VX to VY minus VX. VF is set to 0 when there's a borrow,
+        and 1 when there isn't.'''
         if self.__V[int(self.__opCode[2], 16)] > \
            self.__V[int(self.__opCode[1], 16)]:
             self.__V[15] = 1
@@ -281,8 +281,8 @@ class Chip8(object):
             self.__V[int(self.__opCode[1], 16)]
         self.__pc += 2
 
-    def __inst0x8XYE(self):
-        '''0x8XYE: Shift VX left by one. VF is set to the value of the most
+    def __inst8XYE(self):
+        '''8XYE: Shift VX left by one. VF is set to the value of the most
         significant bit of VX before the shift.'''
         # Check for least significant bit
         if int(self.__V[int(self.__opCode[1], 16)] / 255) > 1:
@@ -293,31 +293,31 @@ class Chip8(object):
             (self.__V[int(self.__opCode[1], 16)] << 1) & 255
         self.__pc += 2
 
-    def __inst0x9XY0(self):
-        '''0x9XY0: Skip the next instruction if VX doesn't equal VY.'''
+    def __inst9XY0(self):
+        '''9XY0: Skip the next instruction if VX doesn't equal VY.'''
         if self.__V[int(self.__opCode[1], 16)] != \
            self.__V[int(self.__opCode[2], 16)]:
             self.__pc += 4
         else:
             self.__pc += 2
 
-    def __inst0xANNN(self):
+    def __instANNN(self):
         '''ANNN: Set I to the address NNN.'''
         self.__I = int(self.__opCode[1:4], 16)
         self.__pc += 2
 
-    def __inst0xBNNN(self):
+    def __instBNNN(self):
         '''BNNN: Jump to the address NNN plus V0.'''
         self.__pc = int(self.__opCode[1:4], 16) + self.__V[0]
 
-    def __inst0xCXNN(self):
+    def __instCXNN(self):
         '''CXNN: Set VX to a random number and NN.'''
         seed()
         self.__V[int(self.__opCode[1], 16)] = \
             (randint(0, 255) & int(self.__opCode[2:4], 16))
         self.__pc += 2
 
-    def __inst0xDXYN(self):
+    def __instDXYN(self):
         '''DXYN: Draw a sprite at coordinate (VX, VY) that has a width of 8
         pixels and a height of N pixels. Each row of 8 pixels is read as
         bit-coded starting from memory location I value doesn't change after
@@ -343,13 +343,11 @@ class Chip8(object):
                 newX = x + j
                 newY = y
                 # Wrap to other side horizontally
-                if newX >= 64:
-                    while newX >= 64:
-                        newX -= 64
+                while newX >= 64:
+                    newX -= 64
                 # Wrap to other side vertically
-                if newY >= 32:
-                    while newY >= 32:
-                        newY -= 32
+                while newY >= 32:
+                    newY -= 32
                 # XOR drawing mode
                 pxVal = int(data[i][j])
                 if pxVal:
@@ -359,7 +357,7 @@ class Chip8(object):
             y += 1  # Move down to draw the next row
         self.__pc += 2
 
-    def __inst0xEX9E(self):
+    def __instEX9E(self):
         '''EX9E: Skip the next instruction if the key stored in VX is
         pressed.'''
         if self.__key[self.__V[int(self.__opCode[1], 16)]]:
@@ -367,7 +365,7 @@ class Chip8(object):
         else:
             self.__pc += 2
 
-    def __inst0xEXA1(self):
+    def __instEXA1(self):
         '''EXA1 Skip the next instruction if the key stored in VX is not
         pressed.'''
         if not self.__key[self.__V[int(self.__opCode[1], 16)]]:
@@ -375,12 +373,12 @@ class Chip8(object):
         else:
             self.__pc += 2
 
-    def __inst0xFX07(self):
+    def __instFX07(self):
         '''FX07: Set VX to the value of the delay timer.'''
         self.__V[int(self.__opCode[1], 16)] = self.__timers[0]
         self.__pc += 2
 
-    def __inst0xFX0A(self):
+    def __instFX0A(self):
         '''FX0A: Wait for a key press and stored the key in VX.'''
         isKeyPressed = False
         for i in range(16):
@@ -392,28 +390,28 @@ class Chip8(object):
             return
         self.__pc += 2
 
-    def __inst0xFX15(self):
+    def __instFX15(self):
         '''FX15: Set the delay timer to VX.'''
         self.__timers[0] = self.__V[int(self.__opCode[1], 16)]
         self.__pc += 2
 
-    def __inst0xFX18(self):
+    def __instFX18(self):
         '''FX18: Set the sound timer to VX.'''
         self.__timers[1] = self.__V[int(self.__opCode[1], 16)]
         self.__pc += 2
 
-    def __inst0xFX1E(self):
+    def __instFX1E(self):
         '''FX1E: Add VX to I.'''
         self.__I += self.__V[int(self.__opCode[1], 16)]
         self.__pc += 2
 
-    def __inst0xFX29(self):
+    def __instFX29(self):
         '''FX29: Set I to the location of the sprite for the character in
         VX.'''
         self.__I = (self.__V[int(self.__opCode[1], 16)] * 5)
         self.__pc += 2
 
-    def __inst0xFX33(self):
+    def __instFX33(self):
         '''FX33: Store the Binary-coded decimal representation of VX at the
         addresses I, I plus 1, and I plus 2.'''
         num = self.__V[int(self.__opCode[1], 16)]
@@ -424,13 +422,13 @@ class Chip8(object):
         self.__ram[self.__I + 2] = hex(num).replace('x', '')
         self.__pc += 2
 
-    def __inst0xFX55(self):
+    def __instFX55(self):
         '''FX55: Store V0 to VX in memory starting at address I.'''
         for i in range(int(self.__opCode[1], 16) + 1):
             self.__ram[self.__I + i] = hex(self.__V[i]).replace('x', '')
         self.__pc += 2
 
-    def __inst0xFX65(self):
+    def __instFX65(self):
         '''FX65: Fill V0 to VX with values from memory starting at address
         I.'''
         for i in range(int(self.__opCode[1], 16) + 1):
